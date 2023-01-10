12df9a6baeaa60780d2aa401c3ed80e4789ac6db
Vuong Nguyen <vuong.nguyen@adthena.com>
2023-01-10 17:18:47 +0000

    update message body

--- producer.py
+++ producer.py
@@ -6,7 +6,7 @@ channel = connection.channel()  # create channel
 
 channel.queue_declare(queue='letterbox')
 
-message = 'Hello, this is my first message'
+message = 'Hello World'
 
 channel.basic_publish(exchange='', routing_key='letterbox', body=message)
 print(f"Sent message: {message}")
